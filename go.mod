8084f523c1ed338b0722d3725ee442b7ce299b2f
woot <worrawutp@gmail.com>
2021-09-02 21:12:46 +0700

    Able to connect database but using the dummy database source

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module worrawutp/good_burger
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,69 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
 )
 
-func ListBurgerHandler(w http.ResponseWriter, r *http.Request) {
-
+type Menu struct {
+	Id          int
+	Name        string
+	Description string
+	Price       int
 }
 
-func BurgerHandler(w http.ResponseWriter, r *http.Request) {
+var Conn *pgx.Conn
+
+// Conn is initialize with zero value
+
+func ListBurgersHandler(w http.ResponseWriter, r *http.Request) {
+	var menus []Menu
+	rows, err := Conn.Query(context.Background(), "select * from menus")
+
+	for rows.Next() {
+		var m = Menu{}
+		rows.Scan(&m.Id, &m.Name, &m.Description, &m.Price)
+		menus = append(menus, m)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%#v\n", menus)
+	menuTemplate, err := template.ParseFiles("views/menus/index.html")
+	if err != nil {
+		http.ServeFile(w, r, "public/500.html")
+		return
+	}
+	menuTemplate.Execute(w, menus)
+}
+
+func BurgersHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.ParseInt(params["id"], 0, 64)
+
+	var menu = Menu{}
+	err := Conn.QueryRow(context.Background(), "select * from menus where id=$1", id).Scan(&menu.Id, &menu.Name, &menu.Description, &menu.Price)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%#v\n", menu)
+	menuTemplate, err := template.ParseFiles("views/menus/show.html")
+	if err != nil {
+		http.ServeFile(w, r, "public/500.html")
+		return
+	}
+	menuTemplate.Execute(w, menu)
 
 }
 
@@ -24,15 +76,31 @@ func NotFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("vim-go")
+	// Load environment variables
+	env := os.Getenv("GOOD_BURGER_ENV")
+	if "" == env {
+		env = "development"
+	}
+	godotenv.Load(".env." + env)
+	godotenv.Load()
+	fmt.Println(os.Getenv("DATABASE_URL"))
 
+	// Connect database
+	var err error
+	Conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Problem! cannot connect to database")
+	}
+	fmt.Println(Conn)
+
+	// Routing
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Go")
 	})
 	router.HandleFunc("/about", AboutHandler)
-	router.HandleFunc("/burgers", ListBurgerHandler)
-	router.HandleFunc("/burgers/{id: [0-9]+}", BurgerHandler)
+	router.HandleFunc("/burgers", ListBurgersHandler)
+	router.HandleFunc("/burgers/{id:[0-9]+}", BurgersHandler)
 	router.NotFoundHandler = http.HandlerFunc(NotFound)
 
 	http.Handle("/", router)
